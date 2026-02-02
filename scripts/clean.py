855740610cc51a8257536307db397d5869cb6f69
Niema Moshiri <niemamoshiri@gmail.com>
2026-02-01 22:42:11 -0800

    Update clean.py

--- scripts/clean.py
+++ scripts/clean.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         print("Folder not found: %s" % argv[1], file=stderr); exit(1)
 
     # remove all empty-looking files
-    print("Searching for EFTA*.* files in: %s" % path)
+    print("Cleaning EFTA*.* files in: %s" % path)
     for pdf in tqdm(list(path.rglob('EFTA*.*'))):
         with open(pdf, 'rb') as f:
             prefix = f.read(100)
