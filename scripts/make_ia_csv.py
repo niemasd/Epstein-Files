c0a5d602ee2d04c640e9831804cac634ad484ff9
Niema Moshiri <niemamoshiri@gmail.com>
2026-02-11 11:14:44 -0800

    Added helper script to make Internet Archive upload CSV

---
+++ scripts/make_ia_csv.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+'''
+Make a CSV file for an Internet Archive upload
+'''
+from gzip import open as goen
+from pathlib import Path
+from sys import stderr
+from tqdm import tqdm
+import argparse
+
+# run script
+if __name__ == "__main__":
+    # parse user args
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d', '--directory', required=True, type=str, help="File Directory")
+    parser.add_argument('-mi', '--meta_id', required=False, type=str, default='niema_efta', help="Metadata: Internet Archive ID")
+    parser.add_argument('-mc', '--meta_creator', required=False, type=str, default='U.S. Department of Justice', help="Metadata: Creator")
+    parser.add_argument('-ml', '--meta_language', required=False, type=str, default='English', help="Metadata: Language")
+    parser.add_argument('-mt', '--meta_type', required=False, type=str, default='texts', help="Metadata: Media Type")
+    parser.add_argument('-o', '--output', required=False, type=str, default='stdout', help="Output File (CSV)")
+    args = parser.parse_args()
+    args.directory = Path(args.directory)
+    if not args.directory.is_dir():
+        print("Directory not found: %s" % args.directory, file=stderr); exit(1)
+    if args.output == 'stdout':
+        from sys import stdout as out_f
+    else:
+        args.output = Path(args.output)
+        if args.output.exists():
+            print("Output file exists: %s" % args.output, file=stderr); exit(1)
+        elif args.output.suffix.lower() == '.gz':
+            out_f = gopen(args.output, 'wt')
+        else:
+            out_f = open(args.output, 'wt')
+
+    # enumerate files (for speed)
+    print("Enumerating files in: %s" % args.directory, file=stderr)
+    paths = list(tqdm(args.directory.rglob('*.*')))
+    print("Found %d files" % len(paths), file=stderr)
+
+    # write CSV output
+    print("Writing output CSV: %s" % args.output, file=stderr)
+    out_f.write("identifier,file,creator,language,mediatype\n")
+    for path in tqdm(paths):
+        out_f.write(args.meta_id) # identifier
+        out_f.write(',')
+        out_f.write(str(path.resolve())) # file
+        out_f.write(',')
+        out_f.write(args.meta_creator) # creator
+        out_f.write(',')
+        out_f.write(args.meta_language) # language
+        out_f.write(',')
+        out_f.write(args.meta_type) # mediatype
+        out_f.write('\n')
+    out_f.close()
